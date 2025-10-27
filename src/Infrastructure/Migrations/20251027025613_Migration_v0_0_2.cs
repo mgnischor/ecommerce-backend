1149c7a9f08371d1a1318f192813621e6feec291
Miguel Nischor <miguel@datatower.tech>
2025-10-26 23:00:23 -0400

    migration: Version v0.0.2

---
+++ src/Infrastructure/Migrations/20251027025613_Migration_v0_0_2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.src.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_v0_0_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_by = table.Column<Guid>(type: "uuid", nullable: false, defaultValue: new Guid("ce06e1a8-f688-44b6-b616-4badf09d9153")),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccessLevel = table.Column<int>(type: "integer", nullable: false),
+                    address = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    country = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", unicode: false, maxLength: 255, nullable: false),
+                    password_hash = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    username = table.Column<string>(type: "character varying(50)", unicode: false, maxLength: 50, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    IsBanned = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDebugEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    IsEmailVerified = table.Column<bool>(type: "boolean", nullable: false),
+                    Groups = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
+                    FavoriteProducts = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
+                    birth_date = table.Column<DateTime>(type: "date", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_created_at",
+                schema: "public",
+                table: "users",
+                column: "created_at");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                schema: "public",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_username",
+                schema: "public",
+                table: "users",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users",
+                schema: "public");
+        }
+    }
+}
