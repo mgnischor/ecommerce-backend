eff43d1a558859496a350155b7437ccdefc9c9ee
Miguel Nischor <miguel@datatower.tech>
2025-10-26 22:26:31 -0400

    code: Add PostgresqlContext class for Entity Framework Core integration

---
+++ src/Infrastructure/Persistence/PostgresqlContext.cs
@@ -0,0 +1,24 @@
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Persistence;
+
+class PostgresqlContext : DbContext
+{
+    public PostgresqlContext(DbContextOptions<PostgresqlContext> options)
+        : base(options) { }
+
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PostgresqlContext).Assembly);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Database=ecommerce;Username=ecommerce;Password=ecommerce"
+        );
+    }
+}
