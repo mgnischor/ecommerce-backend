8284257b6f21d727c3efb55b8e77bb361c17ba52
Miguel Nischor <miguel@datatower.tech>
2025-10-27 15:48:26 -0400

    migration: Version 0.0.12

--- src/Infrastructure/SQL/00_complete_migration.sql
+++ src/Infrastructure/SQL/00_complete_migration.sql
@@ -723,5 +723,13 @@ BEGIN
     VALUES ('20251027182346_Migration_v0_0_11', '9.0.10');
     END IF;
 END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20251027194648_Migration_v0_0_12') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20251027194648_Migration_v0_0_12', '9.0.10');
+    END IF;
+END $EF$;
 COMMIT;
 
