bc5035758b71a41080132f373a4a95c9a6091a06
Miguel Nischor <miguel@datatower.tech>
2025-10-27 08:36:24 -0400

    migration: Migration SQL script for v0.0.4

---
+++ src/Infrastructure/SQL/3_20251027033034_Migration_v0_0_4.sql
@@ -0,0 +1,9 @@
+﻿START TRANSACTION;
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251027032829_Migration_v0_0_3', '9.0.10');
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251027033034_Migration_v0_0_4', '9.0.10');
+
+COMMIT;
+
