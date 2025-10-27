55034f680bbeecf44d97595a2f510d91ee319821
Miguel Nischor <miguel@datatower.tech>
2025-10-27 11:17:07 -0400

    code: Add DbSet properties for ChartOfAccounts, JournalEntries, AccountingEntries, and InventoryTransactions in PostgresqlContext

--- src/Infrastructure/Persistence/PostgresqlContext.cs
+++ src/Infrastructure/Persistence/PostgresqlContext.cs
@@ -10,6 +10,14 @@ public class PostgresqlContext : DbContext
 
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<ProductEntity> Products { get; set; }
+    
+    // Contabilidade
+    public DbSet<ChartOfAccountsEntity> ChartOfAccounts { get; set; }
+    public DbSet<JournalEntryEntity> JournalEntries { get; set; }
+    public DbSet<AccountingEntryEntity> AccountingEntries { get; set; }
+    
+    // Estoque
+    public DbSet<InventoryTransactionEntity> InventoryTransactions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
