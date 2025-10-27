80354dcab81a22d2869ebbe83774c0b6577f8afe
Miguel Nischor <miguel@datatower.tech>
2025-10-27 14:25:25 -0400

    migration: Version 0.0.11

---
+++ src/Infrastructure/Migrations/20251027182346_Migration_v0_0_11.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.src.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration_v0_0_11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
