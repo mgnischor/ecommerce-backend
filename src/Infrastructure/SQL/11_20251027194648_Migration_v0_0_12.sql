fad008a044456f4b6d3cd191c3585999409dc35f
Miguel Nischor <miguel@datatower.tech>
2025-10-27 15:48:28 -0400

    migration: Version 0.0.12

---
+++ src/Infrastructure/SQL/11_20251027194648_Migration_v0_0_12.sql
@@ -0,0 +1,9 @@
+﻿START TRANSACTION;
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251027182346_Migration_v0_0_11', '9.0.10');
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20251027194648_Migration_v0_0_12', '9.0.10');
+
+COMMIT;
+
