858c532f74814df200e6a57dd8859fda844ba738
Miguel Nischor <miguel@nischor.com.br>
2025-11-02 20:17:34 -0400

    code: add PromotionType enum for categorizing promotion types in the e-commerce system

---
+++ src/Domain/Enums/PromotionType.cs
@@ -0,0 +1,42 @@
+namespace ECommerce.Domain.Enums;
+
+/// <summary>
+/// Represents the type of promotion
+/// </summary>
+public enum PromotionType
+{
+    /// <summary>
+    /// Percentage discount
+    /// </summary>
+    PercentageDiscount = 0,
+
+    /// <summary>
+    /// Fixed amount discount
+    /// </summary>
+    FixedAmountDiscount = 1,
+
+    /// <summary>
+    /// Buy X get Y free
+    /// </summary>
+    BuyXGetYFree = 2,
+
+    /// <summary>
+    /// Free shipping
+    /// </summary>
+    FreeShipping = 3,
+
+    /// <summary>
+    /// Bundle deal
+    /// </summary>
+    Bundle = 4,
+
+    /// <summary>
+    /// Flash sale
+    /// </summary>
+    FlashSale = 5,
+
+    /// <summary>
+    /// Clearance sale
+    /// </summary>
+    Clearance = 6
+}
