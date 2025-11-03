afbf30364080064d35bc42c9e2068a31b8456eef
Miguel Nischor <miguel@nischor.com.br>
2025-11-02 22:30:39 -0400

    build: add global using directives for test project

---
+++ tests/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives for test project
+global using System;
+global using System.Threading.Tasks;
+global using ECommerce.Application.Interfaces;
+global using ECommerce.Application.Services;
+global using ECommerce.Domain.Entities;
+global using ECommerce.Domain.Interfaces;
+global using FluentAssertions;
+global using Microsoft.Extensions.Logging;
+global using Moq;
+global using NUnit.Framework;
