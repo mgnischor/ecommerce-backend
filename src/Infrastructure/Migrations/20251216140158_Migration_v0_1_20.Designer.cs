e247d874708c7bd5aaa91a71b3e446526aa651a9
Miguel Nischor <miguel@nischor.com.br>
2025-12-17 11:08:50 -0400

    migration: Version 0.1.20

--- src/Infrastructure/Migrations/20251216140158_Migration_v0_1_20.Designer.cs
+++ src/Infrastructure/Migrations/20251216140158_Migration_v0_1_20.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using ECommerce.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ECommerce.src.Infrastructure.Migrations
 {
     [DbContext(typeof(PostgresqlContext))]
-    partial class PostgresqlContextModelSnapshot : ModelSnapshot
+    [Migration("20251216140158_Migration_v0_1_20")]
+    partial class Migration_v0_1_20
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
