1b266b6d09466f838bb29ed2edf7fa947eac5cc7
Miguel Nischor <miguel@datatower.tech>
2025-12-31 10:47:09 -0300

    code: Remove FluentAssertions global using directive from GlobalUsings.cs

--- tests/GlobalUsings.cs
+++ tests/GlobalUsings.cs
@@ -11,7 +11,6 @@ global using ECommerce.Domain.Interfaces;
 global using ECommerce.Domain.Services;
 global using ECommerce.Infrastructure.Repositories;
 global using ECommerce.Tests.TestFixtures;
-global using FluentAssertions;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;
 global using Moq;
