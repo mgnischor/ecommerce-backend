75c1cd7ec198fed49615b9c43fd5ec8c5cb1e255
Miguel Nischor <miguel@nischor.com.br>
2026-06-14 18:39:24 -0400

    migration: v0.1.24

--- src/Infrastructure/Migrations/PostgresqlContextModelSnapshot.cs
+++ src/Infrastructure/Migrations/PostgresqlContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace ECommerce.src.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.5")
+                .HasAnnotation("ProductVersion", "10.0.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
