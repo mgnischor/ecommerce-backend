8b9685e4b834e4ea8764767e9e3ee822b4f816bf
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:25:16 -0400

    refactor(infrastructure): rename ECommerce namespace to Comex in src/Infrastructure/Repositories/InventoryTransactionRepository.cs

--- src/Infrastructure/Repositories/InventoryTransactionRepository.cs
+++ src/Infrastructure/Repositories/InventoryTransactionRepository.cs
@@ -1,11 +1,11 @@
-using ECommerce.Application.Interfaces;
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.Enums;
-using ECommerce.Domain.Interfaces;
-using ECommerce.Infrastructure.Persistence;
+using Comex.Application.Interfaces;
+using Comex.Domain.Entities;
+using Comex.Domain.Enums;
+using Comex.Domain.Interfaces;
+using Comex.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
-namespace ECommerce.Infrastructure.Repositories;
+namespace Comex.Infrastructure.Repositories;
 
 /// <summary>
 /// Repository implementation for managing inventory transaction data access operations.
