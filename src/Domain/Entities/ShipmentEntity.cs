498544f4ec4b45a0bfc58e258175c13c7f107ef5
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:47 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Entities/ShipmentEntity.cs

--- src/Domain/Entities/ShipmentEntity.cs
+++ src/Domain/Entities/ShipmentEntity.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Enums;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Entities;
+namespace Comex.Domain.Entities;
 
 /// <summary>
 /// Represents a shipment in the e-commerce system.
