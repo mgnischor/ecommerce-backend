db6bd105e1e58c92fd828874134cdf0d47c7fd6b
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:42 -0400

    refactor(application): rename ECommerce namespace to Comex in src/Application/Services/LoggingService.cs

--- src/Application/Services/LoggingService.cs
+++ src/Application/Services/LoggingService.cs
@@ -1,7 +1,7 @@
-using ECommerce.Application.Interfaces;
+using Comex.Application.Interfaces;
 using Microsoft.Extensions.Logging;
 
-namespace ECommerce.Application.Services;
+namespace Comex.Application.Services;
 
 /// <summary>
 /// Service for centralized logging across the application
