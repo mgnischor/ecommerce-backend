46978afe6e5465de8fb0ba4c864611628ee249c0
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:44 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Entities/ChartOfAccountsEntity.cs

--- src/Domain/Entities/ChartOfAccountsEntity.cs
+++ src/Domain/Entities/ChartOfAccountsEntity.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Enums;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Entities;
+namespace Comex.Domain.Entities;
 
 /// <summary>
 /// Represents an accounting account in the Chart of Accounts.
