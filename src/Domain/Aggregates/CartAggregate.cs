7620d9d7b0f9aaf107ec173d031b2df3522ebf47
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:43 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Aggregates/CartAggregate.cs

--- src/Domain/Aggregates/CartAggregate.cs
+++ src/Domain/Aggregates/CartAggregate.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Entities;
+using Comex.Domain.Entities;
 
-namespace ECommerce.Domain.Aggregates;
+namespace Comex.Domain.Aggregates;
 
 /// <summary>
 /// Shopping cart aggregate root - encapsulates cart business logic
