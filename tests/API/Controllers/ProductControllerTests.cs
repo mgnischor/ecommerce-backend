d6a760e7eb7e2f2d686af604696eba238bb30c21
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:25:19 -0400

    test: rename ECommerce namespace to Comex in tests/API/Controllers/ProductControllerTests.cs

--- tests/API/Controllers/ProductControllerTests.cs
+++ tests/API/Controllers/ProductControllerTests.cs
@@ -1,10 +1,10 @@
-using ECommerce.API.Controllers;
-using ECommerce.Domain.Enums;
-using ECommerce.Infrastructure.Persistence;
+using Comex.API.Controllers;
+using Comex.Domain.Enums;
+using Comex.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ECommerce.Tests.API.Controllers;
+namespace Comex.Tests.API.Controllers;
 
 /// <summary>
 /// Tests for ProductController
