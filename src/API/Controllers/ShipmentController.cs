d68797cbd45e10bf661b008e62c82a9cb57aa676
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:34 -0400

    refactor(api): rename ECommerce namespace to Comex in src/API/Controllers/ShipmentController.cs

--- src/API/Controllers/ShipmentController.cs
+++ src/API/Controllers/ShipmentController.cs
@@ -1,16 +1,16 @@
 using System.Security.Claims;
 using System.Text.RegularExpressions;
-using ECommerce.API.Constants;
-using ECommerce.Application.Interfaces;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.Policies;
-using ECommerce.Infrastructure.Persistence;
+using Comex.API.Constants;
+using Comex.Application.Interfaces;
+using Comex.Application.Services;
+using Comex.Domain.Entities;
+using Comex.Domain.Policies;
+using Comex.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ECommerce.API.Controllers;
+namespace Comex.API.Controllers;
 
 /// <summary>
 /// Provides HTTP endpoints for managing order shipments and tracking delivery logistics.
