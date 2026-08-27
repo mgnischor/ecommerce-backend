1354f5c4cc711d022eed3a623850ce0e37e6b11a
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:37 -0400

    refactor(api): rename ECommerce namespace to Comex in src/API/DTOs/LoginRequestDto.cs

--- src/API/DTOs/LoginRequestDto.cs
+++ src/API/DTOs/LoginRequestDto.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace ECommerce.API.DTOs;
+namespace Comex.API.DTOs;
 
 /// <summary>
 /// Data Transfer Object (DTO) for user authentication login requests
