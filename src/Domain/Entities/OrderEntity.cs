2d7f5aa1b5bd233507521a22ee3b9cd7ddc9b38d
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:46 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Entities/OrderEntity.cs

--- src/Domain/Entities/OrderEntity.cs
+++ src/Domain/Entities/OrderEntity.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Enums;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Entities;
+namespace Comex.Domain.Entities;
 
 /// <summary>
 /// Represents a customer order in the e-commerce system.
