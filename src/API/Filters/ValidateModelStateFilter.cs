15b21122b6a3638069623aa24fea3eb10e531376
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:39 -0400

    refactor(api): rename ECommerce namespace to Comex in src/API/Filters/ValidateModelStateFilter.cs

--- src/API/Filters/ValidateModelStateFilter.cs
+++ src/API/Filters/ValidateModelStateFilter.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace ECommerce.API.Filters;
+namespace Comex.API.Filters;
 
 /// <summary>
 /// Action filter that validates model state before controller action execution
