6d75a22d6011407c8bc8a057e0cc2e8667ca58d0
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:58 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Policies/OrderFulfillmentPolicy.cs

--- src/Domain/Policies/OrderFulfillmentPolicy.cs
+++ src/Domain/Policies/OrderFulfillmentPolicy.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Enums;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Policies;
+namespace Comex.Domain.Policies;
 
 /// <summary>
 /// Defines business rules for order fulfillment and shipment planning
