c0123f45a17c4784b5b76d9e210b29b5c0f4e248
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:34 -0400

    refactor(api): rename ECommerce namespace to Comex in src/API/Controllers/RewardsController.cs

--- src/API/Controllers/RewardsController.cs
+++ src/API/Controllers/RewardsController.cs
@@ -1,15 +1,15 @@
-using ECommerce.API.Constants;
-using ECommerce.API.DTOs;
-using ECommerce.Application.Interfaces;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.Policies;
-using ECommerce.Infrastructure.Persistence;
+using Comex.API.Constants;
+using Comex.API.DTOs;
+using Comex.Application.Interfaces;
+using Comex.Application.Services;
+using Comex.Domain.Entities;
+using Comex.Domain.Policies;
+using Comex.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ECommerce.API.Controllers;
+namespace Comex.API.Controllers;
 
 /// <summary>
 /// Provides HTTP endpoints for managing the customer loyalty rewards program.
