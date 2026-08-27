6414e94c7188132b6f5add79957b746c81bcf6d6
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:48 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Entities/VendorEntity.cs

--- src/Domain/Entities/VendorEntity.cs
+++ src/Domain/Entities/VendorEntity.cs
@@ -1,6 +1,6 @@
-using ECommerce.Domain.Enums;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Entities;
+namespace Comex.Domain.Entities;
 
 /// <summary>
 /// Represents a vendor/seller in the e-commerce system.
