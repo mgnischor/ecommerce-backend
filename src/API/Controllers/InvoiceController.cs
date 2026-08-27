c329220ec59c0171822e3b8770faf39e183ea7ff
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:33 -0400

    refactor(api): rename ECommerce namespace to Comex in src/API/Controllers/InvoiceController.cs

--- src/API/Controllers/InvoiceController.cs
+++ src/API/Controllers/InvoiceController.cs
@@ -1,16 +1,16 @@
 using System.Security.Claims;
-using ECommerce.API.Constants;
-using ECommerce.API.DTOs;
-using ECommerce.Application.Interfaces;
-using ECommerce.Application.Services;
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.Policies;
-using ECommerce.Infrastructure.Persistence;
+using Comex.API.Constants;
+using Comex.API.DTOs;
+using Comex.Application.Interfaces;
+using Comex.Application.Services;
+using Comex.Domain.Entities;
+using Comex.Domain.Policies;
+using Comex.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace ECommerce.API.Controllers;
+namespace Comex.API.Controllers;
 
 /// <summary>
 /// Provides HTTP endpoints for managing invoices.
