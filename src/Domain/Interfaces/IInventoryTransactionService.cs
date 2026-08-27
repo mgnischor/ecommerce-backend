ee909ad415299a31e225d37ea9061abfe72bcf27
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:24:55 -0400

    refactor(domain): rename ECommerce namespace to Comex in src/Domain/Interfaces/IInventoryTransactionService.cs

--- src/Domain/Interfaces/IInventoryTransactionService.cs
+++ src/Domain/Interfaces/IInventoryTransactionService.cs
@@ -1,7 +1,7 @@
-using ECommerce.Domain.Entities;
-using ECommerce.Domain.Enums;
+using Comex.Domain.Entities;
+using Comex.Domain.Enums;
 
-namespace ECommerce.Domain.Interfaces;
+namespace Comex.Domain.Interfaces;
 
 /// <summary>
 /// Interface for inventory transaction management
