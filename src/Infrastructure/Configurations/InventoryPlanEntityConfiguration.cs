de2c071d1b46690d8795f1ffadb0e55e6617fae4
Miguel Nischor <miguel@nischor.com.br>
2026-08-27 10:25:04 -0400

    refactor(infrastructure): rename ECommerce namespace to Comex in src/Infrastructure/Configurations/InventoryPlanEntityConfiguration.cs

--- src/Infrastructure/Configurations/InventoryPlanEntityConfiguration.cs
+++ src/Infrastructure/Configurations/InventoryPlanEntityConfiguration.cs
@@ -1,8 +1,8 @@
-using ECommerce.Domain.Entities;
+using Comex.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace ECommerce.Infrastructure.Configurations;
+namespace Comex.Infrastructure.Configurations;
 
 /// <summary>
 /// Configuration for the InventoryPlanEntity.
